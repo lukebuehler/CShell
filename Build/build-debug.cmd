4d757a587d61ef31298edf238d1f1d64029a7556
Lukas Buhler <lukas.buehler@gmail.com>
2014-05-09 22:54:21 -0400

    changed build scripts to VS 2013

--- Build/build-debug.cmd
+++ Build/build-debug.cmd
@@ -1,3 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 
 MSBuild ../Src/CShell.sln /t:Build /p:Configuration=Debug

46ff555cae51a32604058f77deaaa99bea975331
Lukas Buhler <lukas.buehler@gmail.com>
2015-11-02 09:50:53 -0500

    removed license headers in all files

--- Src/CShell/Modules/Workspace/Views/AssemblyGacView.xaml.cs
+++ Src/CShell/Modules/Workspace/Views/AssemblyGacView.xaml.cs
@@ -1,34 +1,8 @@
-﻿#region License
-// CShell, A Simple C# Scripting IDE
-// Copyright (C) 2013  Arnova Asset Management Ltd., Lukas Buhler
-// 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#endregion
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CShell.Modules.Workspace.Views
 {

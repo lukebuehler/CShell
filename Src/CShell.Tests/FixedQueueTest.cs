64144486174aefe8968fa577e4feac6e8a52ba94
medmondson <eddy556@gmail.com>
2016-01-16 00:32:20 +0000

    Fixed queue taking shape (need sleep)

---
+++ Src/CShell.Tests/FixedQueueTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CShell.Tests
+{
+    public class FixedQueueTest
+    {
+        [Test]
+        public void AddItemToQueue_Limit1_ExpectAdded()
+        {
+           var queue = new FixedQueue<string>(1);
+           const string expected = "Test string"; 
+
+           queue.Add(expected);
+
+           List<string> actual = queue.Contents().ToList();
+
+           Assert.AreEqual(expected, actual[0]);
+
+        }
+    } 
+
+    public class FixedQueue<T>
+    {
+        private readonly int _limit;
+        readonly ConcurrentQueue<T> _queue = new ConcurrentQueue<T>(); 
+
+        public FixedQueue(int limit)
+        {
+            _limit = limit;
+        }
+
+        public void Add(T item)
+        {
+            _queue.Enqueue(item);
+        }
+
+        public IEnumerable<T> Contents()
+        {
+            return _queue;
+        }
+    }
+}

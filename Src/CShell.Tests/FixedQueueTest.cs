1038aa981ab699384fb6597f22b1fb734c3869ce
medmondson <eddy556@gmail.com>
2016-01-19 23:50:56 +0000

    Finishing off fixed queue.  Next step to integrate with REPL.

--- Src/CShell.Tests/FixedQueueTest.cs
+++ Src/CShell.Tests/FixedQueueTest.cs
@@ -19,7 +19,36 @@ namespace CShell.Tests
            List<string> actual = queue.Contents().ToList();
 
            Assert.AreEqual(expected, actual[0]);
+        }
 
+        [Test]
+        public void AddMultipleItemsToQueue_Limit1_ExpectLimitAdhered()
+        {
+            const int expectedQueueLength = 1;
+           
+            var queue = new FixedQueue<string>(expectedQueueLength);
+
+            queue.Add("Test String 1");
+            queue.Add("Test String 2");
+
+            int actualQueueLength = queue.Contents().Count();
+
+            Assert.AreEqual(expectedQueueLength,actualQueueLength);
+        }
+
+        [Test]
+        public void AddMultipleItemsToQueue_Limit1_ExpectLastElementKept()
+        {
+            const string expectedElementKept = "Test String 2";
+            
+            var queue = new FixedQueue<string>(1);
+
+            queue.Add("Test String 1");
+            queue.Add("Test String 2");
+
+            string actualElementKept = queue.Contents().Single();
+
+            Assert.AreEqual(expectedElementKept, actualElementKept);
         }
     } 
 
@@ -36,6 +65,13 @@ namespace CShell.Tests
         public void Add(T item)
         {
             _queue.Enqueue(item);
+
+            lock (this) //ensure threads don't step on each other
+            {
+                T overflow;
+                while (_queue.Count > _limit && _queue.TryDequeue(out overflow)) ;
+            }
+            
         }
 
         public IEnumerable<T> Contents()
